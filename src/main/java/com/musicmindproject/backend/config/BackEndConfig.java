343ef20a04dd988582c54505abb7e56bd1bc39b5
Banana <lillidilly@gmail.com>
2017-11-08 20:29:47 +0100

    changed application path

--- src/main/java/com/musicmindproject/backend/config/BackEndConfig.java
+++ src/main/java/com/musicmindproject/backend/config/BackEndConfig.java
@@ -3,6 +3,5 @@ package com.musicmindproject.backend.config;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("backend")
 public class BackEndConfig extends Application {
 }
--- src/main/java/com/musicmindproject/backend/servlets/ContactServlet.java
+++ src/main/java/com/musicmindproject/backend/servlets/ContactServlet.java
@@ -1,54 +1,68 @@
 package com.musicmindproject.backend.servlets;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.Properties;
 
-@WebServlet("contact")
+@WebServlet("/contact")
 public class ContactServlet extends HttpServlet {
+
+    private static final String USERNAME = "musicmindproject@gmail.com";
+    private static final String PASSWORD = "PNnTz8My";
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
         //Parameters
         String name = req.getParameter("name");
         String email = req.getParameter("email");
+        String subject = req.getParameter("subject");
         String comment = req.getParameter("comment");
 
-        //Own data
-        String mail = "musicmindproject@gmail.com";
-        String pw = "PNnTz8My";
-
-        //mailserver data
-        String hostServer = "smtp.gmail.com";
-        String hostPort = "587";
-
         //sending the email
-        Properties prop = new Properties();
-        prop.setProperty("mail.smtp.host", hostServer);
-        prop.setProperty("mail.user", mail);
-        prop.setProperty("mail.password", pw);
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.port", 587);
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true");
 
-        Session session = Session.getDefaultInstance(prop);
-
+        Authenticator auth = new SMTPAuthenticator();
+        Session session = Session.getInstance(props, auth);
+        Transport transport = null;
         try {
+            transport = session.getTransport();
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(mail);
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(mail));
-            message.setSubject("Contact from " + name);
-            message.setText("Mail from "+ email + "\n" + comment);
-            Transport.send(message);
-        }
-        catch (MessagingException e) {
+            message.setSubject("Comment by " + name + ": " + subject);
+            message.setContent("Mail from : " + email + "\n" + comment, "text/plain");
+            message.setFrom(new InternetAddress(USERNAME));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(USERNAME));
+            transport.connect();
+            transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+            transport.close();
+            System.out.println("Successfully sent contact information");
+        } catch (NoSuchProviderException e) {
             e.printStackTrace();
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SMTPAuthenticator extends javax.mail.Authenticator {
+        public PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(USERNAME, PASSWORD);
         }
     }
 }
