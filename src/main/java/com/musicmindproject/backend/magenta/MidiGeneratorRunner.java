1169b2fe517bfef8cd3ad006ee7e2db8653ad1fd
sev-b <bergerseverin1504@gmail.com>
2018-04-06 14:50:55 +0200

    changed to just creating dummy networks

--- src/main/java/com/musicmindproject/backend/magenta/MidiGeneratorRunner.java
+++ src/main/java/com/musicmindproject/backend/magenta/MidiGeneratorRunner.java
@@ -1,5 +1,6 @@
 package com.musicmindproject.backend.magenta;
 
+import com.musicmindproject.backend.entities.enums.Instrument;
 import com.musicmindproject.backend.entities.enums.MusicGenre;
 
 import javax.annotation.PostConstruct;
@@ -19,8 +20,9 @@ public class MidiGeneratorRunner {
     private static final String RUN_DIRECTORY = "/mnt/sequences_tmp/melody_rnn/logdir/run_%s";
     private static final String OUTPUT_DIRECTORY = "/mnt/sequences_tmp/melody_rnn/generated_tracks/%s";
     private static final String SEQUENCE_EXAMPLE_FILE = "/mnt/sequence_examples/%s/training_melodies.tfrecord";
+    private static final String COUNTRY_SEQUENCE_EXAMPLE_FILE = "/mnt/sequence_examples/country/training_melodies.tfrecord";
     private static final String WORKING_DIRECTORY = "/root/magenta";
-    private static final int NUM_TRAINING_STEPS = 200;
+    private static final int NUM_TRAINING_STEPS = 1;
     private static final int NUM_RUN_STEPS = 128;
 
     /**
@@ -28,46 +30,34 @@ public class MidiGeneratorRunner {
      */
 
     private static MidiGeneratorRunner instance;
+
     public static MidiGeneratorRunner getInstance() {
-        if(instance == null) instance = new MidiGeneratorRunner();
+        if (instance == null) instance = new MidiGeneratorRunner();
         return instance;
     }
 
-    private MidiGeneratorRunner() {  }
+    private MidiGeneratorRunner() {
+    }
 
     int exitStatus;
 
     @PostConstruct
     public void init() {
-//        try {
-//            Process activationProcess = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta"}, null, new File(WORKING_DIRECTORY));
-//            exitStatus = activationProcess.waitFor();
-//            System.out.println("ACTIVATED MAGENTA");
-//        } catch (IOException | InterruptedException e) {
-//            System.err.println(e.getLocalizedMessage());
-//            exitStatus = -1;
-//        }
-
         for (MusicGenre genre : MusicGenre.values()) {
-
-            String currName = genre.name().toLowerCase();
-
-            //JUST FOR TESTING
-            if (genre == MusicGenre.COUNTRY) {
-
-                //for (Instrument instrument : genre.getInstruments()) {
-                    Thread toRun = new Thread(() -> {
-                        try {
-                            while (true) {
+            for (Instrument instrument : genre.getInstruments()) {
+                String currName = genre.name().toLowerCase() + "_" + instrument.name().toLowerCase();
+                Thread toRun = new Thread(() -> {
+                    try {
+                        //while (true) {
 //                            if (exitStatus == 0) {
-                                if (!Files.exists(Paths.get(String.format(TFRECORD_FILE, currName)))) {
-                                    System.out.println("NO DATASET FOR " + genre.name().toLowerCase() /*+ "/" + instrument.name()*/ + " FOUND, CREATING...");
+                                /*if (!Files.exists(Paths.get(String.format(TFRECORD_FILE, currName)))) {
+                                    System.out.println("NO DATASET FOR " + genre.name().toLowerCase() + "/" + instrument.name() + " FOUND, CREATING...");
                                     Process datasetGenerator = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta; bazel run //magenta/scripts:convert_dir_to_note_sequences -- \\" +
                                             " --input_dir=" + String.format(AVAILABLE_MUSIC, currName) + " \\" +
                                             " --output_file=" + String.format(TFRECORD_FILE, currName) + " \\" +
                                             " --recursive"}, null, new File(WORKING_DIRECTORY));
                                     datasetGenerator.waitFor();
-                                    System.out.println("CREATED DATASET FOR " + currName /*+ "/" + instrument.name()*/);
+                                    System.out.println("CREATED DATASET FOR " + currName + "/" + instrument.name());
 
                                 }
                                 if(!Files.exists(Paths.get(String.format(GENERATED_DATASET, currName)))){
@@ -77,54 +67,54 @@ public class MidiGeneratorRunner {
                                             " --output_dir=" + String.format(GENERATED_DATASET, currName)+ " \\" +
                                             " --eval_ratio=0.10"}, null, new File(WORKING_DIRECTORY));
                                     datasetGenerator.waitFor();
-                                    System.out.println("CREATED EXAMPLE FOR " + currName /*+ "/" + instrument.name()*/);
-                                }
+                                    System.out.println("CREATED EXAMPLE FOR " + currName + "/" + instrument.name());
+                                }*/
+                        if (!Files.exists(Paths.get(String.format(RUN_DIRECTORY, currName)))) {
+                            Process networkTrainer = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta && bazel run //magenta/models/melody_rnn:melody_rnn_train -- \\" +
+                                    " --config=attention_rnn \\" +
+                                    " --run_dir=" + String.format(RUN_DIRECTORY, currName) + " \\" +
+                                    " --sequence_example_file=" + String.format(COUNTRY_SEQUENCE_EXAMPLE_FILE, currName) + " \\" +
+                                    " --hparams=\"batch_size=64,rnn_layer_sizes=[64,64]\" \\" +
+                                    " --num_training_steps=" + NUM_TRAINING_STEPS}, null, new File(WORKING_DIRECTORY));
+                            networkTrainer.waitFor();
+                        }
 
-                                //noinspection InfiniteLoopStatement
+                        //noinspection InfiniteLoopStatement
 
-                                    Process magentaCommand;
-                                    if(!Files.exists(Paths.get(String.format(OUTPUT_DIRECTORY, currName)))) {
+                        Process magentaCommand;
+                                    /*if(!Files.exists(Paths.get(String.format(OUTPUT_DIRECTORY, currName)))) {
                                         File dir = new File(String.format(OUTPUT_DIRECTORY, currName));
                                         if(!dir.mkdir())
                                             break;
-                                    }
+                                    }*/
 
-                                    if (Objects.requireNonNull(new File(String.format(OUTPUT_DIRECTORY, currName)).listFiles()).length < 50) {
-                                        magentaCommand = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta && bazel run //magenta/models/melody_rnn:melody_rnn_train -- \\" +
-                                                " --config=attention_rnn \\" +
-                                                " --run_dir=" + String.format(RUN_DIRECTORY, currName) + " \\" +
-                                                " --sequence_example_file=" + String.format(SEQUENCE_EXAMPLE_FILE, currName) + " \\" +
-                                                " --hparams=\"batch_size=64,rnn_layer_sizes=[64,64]\" \\" +
-                                                " --num_training_steps=" + NUM_TRAINING_STEPS}, null, new File(WORKING_DIRECTORY));
+                        if (Objects.requireNonNull(new File(String.format(OUTPUT_DIRECTORY, currName)).listFiles()).length < 10) {
+                            magentaCommand = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta && bazel run //magenta/models/melody_rnn:melody_rnn_generate -- \\" +
+                                    " --config=attention_rnn \\" +
+                                    " --run_dir=" + String.format(RUN_DIRECTORY, currName) + " \\" +
+                                    " --output_dir=" + String.format(OUTPUT_DIRECTORY, currName) + " \\" +
+                                    " --num_outputs=1 \\" +
+                                    " --num_steps=" + NUM_RUN_STEPS + " \\" +
+                                    " --hparams=\"batch_size=64,rnn_layer_sizes=[64,64]\" \\" +
+                                    " --primer_melody=\"[60]\""}, null, new File(WORKING_DIRECTORY));
+                            magentaCommand.waitFor();
+                            System.out.println("1 FILE FOR " + genre.name().toLowerCase() + " CREATED");
 
-                                        //System.out.println("TRAINING FOR " + genre.name() + " FINISHED (" + NUM_TRAINING_STEPS + " steps)");
-                                    } else {
-                                        magentaCommand = Runtime.getRuntime().exec(new String[]{"bash", "-c", "source activate magenta && bazel run //magenta/models/melody_rnn:melody_rnn_generate -- \\" +
-                                                " --config=attention_rnn \\" +
-                                                " --run_dir=" + String.format(RUN_DIRECTORY, currName) + " \\" +
-                                                " --output_dir=" + String.format(OUTPUT_DIRECTORY, currName) + " \\" +
-                                                " --num_outputs=1 \\" +
-                                                " --num_steps=" + NUM_RUN_STEPS + " \\" +
-                                                " --hparams=\"batch_size=64,rnn_layer_sizes=[64,64]\" \\" +
-                                                " --primer_melody=\"[60]\""}, null, new File(WORKING_DIRECTORY));
-                                        //System.out.println("1 FILE FOR " + genre.name().toLowerCase() + " CREATED");
-                                    }
+                        } else {
 
-                                    magentaCommand.waitFor();
-                                }
 
-//                            } else {
-//                                System.err.println("Error: Code " + exitStatus);
-//                            }
-                        } catch (IOException | InterruptedException e) {
-                            System.err.println("Could not start magenta! No training or generating will be done. Reason: " + e.getLocalizedMessage());
-                            e.printStackTrace();
-                            System.err.println();
+                            //System.out.println("TRAINING FOR " + genre.name() + " FINISHED (" + NUM_TRAINING_STEPS + " steps)");
                         }
-                    });
 
-                    toRun.start();
-                //}
+
+                        //}
+                    } catch (IOException | InterruptedException e) {
+                        System.err.println("Could not start magenta! No training or generating will be done. Reason: " + e.getLocalizedMessage());
+                        e.printStackTrace();
+                        System.err.println();
+                    }
+                });
+                toRun.start();
             }
         }
     }
