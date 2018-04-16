63d45d3626b7215252ab9f277bd4c75a0cb397ac
sev-b <bergerseverin1504@gmail.com>
2018-04-16 14:38:12 +0200

    new whil(true)

--- src/main/java/com/musicmindproject/backend/magenta/MidiGeneratorRunner.java
+++ src/main/java/com/musicmindproject/backend/magenta/MidiGeneratorRunner.java
@@ -6,14 +6,12 @@ import com.musicmindproject.backend.entities.enums.MusicGenre;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-//@Startup
-//@Singleton
+@Startup
+@Singleton
 public class MidiGeneratorRunner {
     private static final String AVAILABLE_MUSIC = "/mnt/midifiles/%s";
     private static final String GENERATED_DATASET = "/mnt/sequence_examples/%s";
@@ -24,7 +22,7 @@ public class MidiGeneratorRunner {
     private static final String COUNTRY_SEQUENCE_EXAMPLE_FILE = "/mnt/sequence_examples/country/training_melodies.tfrecord";
     private static final String WORKING_DIRECTORY = "/root/magenta";
     private static final int NUM_TRAINING_STEPS = 1;
-    private static final int NUM_RUN_STEPS = 128;
+    private static final int NUM_RUN_STEPS = 1;
 
     /**
      * Runs commands to generate music and to train the neural network
@@ -48,62 +46,31 @@ public class MidiGeneratorRunner {
         for (MusicGenre genre : MusicGenre.values()) {
             for (Instrument instrument : genre.getInstruments()) {
                 String currName = genre.name().toLowerCase() + "_" + instrument.name().toLowerCase();
-                try {
-                    System.out.println("Name of network: " + currName);
-                    Process networkTrainer = Runtime.getRuntime().exec("echo test");
-                    if (Files.notExists(Paths.get(String.format(RUN_DIRECTORY, currName)))) {
-                        System.out.println("Make RUNDIR for " + currName);
-                        networkTrainer = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "sudo bash make_rundir.bash " + currName}, null, new File(WORKING_DIRECTORY));
-                        //networkTrainer.waitFor();
-                        System.out.println("RUNDIR FOR " + currName + " CREATED");
-                    }
-
-                    BufferedReader stdInput = new BufferedReader(new
-                            InputStreamReader(networkTrainer.getInputStream()));
-
-                    BufferedReader stdError = new BufferedReader(new
-                            InputStreamReader(networkTrainer.getErrorStream()));
-                    System.out.println("Here is the standard output of the command:\n");
-                    String s = null;
-                    while ((s = stdInput.readLine()) != null) {
-                        System.out.println(s);
-                    }
-                    System.out.println("Here is the standard error of the command (if any):\n");
-                    while ((s = stdError.readLine()) != null) {
-                        System.out.println(s);
-                    }
-                    networkTrainer.waitFor();
-
-                    Process magentaCommand = Runtime.getRuntime().exec("echo test");
-
-                    if (Files.notExists(Paths.get(String.format(OUTPUT_DIRECTORY, currName))) || new File(String.format(OUTPUT_DIRECTORY, currName)).listFiles().length < 10) {
-                        System.out.println("Generate 1 file for " + currName);
-                        magentaCommand = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "sudo bash generate_file.bash " + currName}, null, new File(WORKING_DIRECTORY));
-                        //magentaCommand.waitFor();
-                        System.out.println("1 FILE FOR " + currName + " CREATED");
-                    }
-
-
-                    stdInput = new BufferedReader(new
-                            InputStreamReader(magentaCommand.getInputStream()));
-
-                    stdError = new BufferedReader(new
-                            InputStreamReader(magentaCommand.getErrorStream()));
-                    System.out.println("Here is the standard output of the command:\n");
-                    s = null;
-                    while ((s = stdInput.readLine()) != null) {
-                        System.out.println(s);
-                    }
-                    System.out.println("Here is the standard error of the command (if any):\n");
-                    while ((s = stdError.readLine()) != null) {
-                        System.out.println(s);
-                    }
-                    magentaCommand.waitFor();
-                } catch (Exception e) {
-                    System.err.println("Could not start magenta! No training or generating will be done. Reason: " + e.getLocalizedMessage());
-                    e.printStackTrace();
-                    System.err.println();
-                }
+                Thread toRun = new Thread(() -> {
+                    try {
+                        while (true) {
+                            //System.out.println("Name of network: " + currName);
+                            Process networkTrainer = Runtime.getRuntime().exec("echo test");
+                            if (Files.notExists(Paths.get(String.format(RUN_DIRECTORY, currName)))) {
+                                //System.out.println("Make RUNDIR for " + currName);
+                                networkTrainer = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "sudo bash make_rundir.bash " + currName + " " + NUM_TRAINING_STEPS}, null, new File(WORKING_DIRECTORY));
+                                networkTrainer.waitFor();
+                                //System.out.println("RUNDIR FOR " + currName + " CREATED");
+                            }
+                            Process magentaCommand = Runtime.getRuntime().exec("echo test");
+                            if (Files.notExists(Paths.get(String.format(OUTPUT_DIRECTORY, currName))) || new File(String.format(OUTPUT_DIRECTORY, currName)).listFiles().length < 10) {
+                                //System.out.println("Generate 1 file for " + currName);
+                                magentaCommand = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", "sudo bash generate_file.bash " + currName + " " + NUM_RUN_STEPS}, null, new File(WORKING_DIRECTORY));
+                                magentaCommand.waitFor();
+                                //System.out.println("1 FILE FOR " + currName + " CREATED");
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Could not start magenta! No training or generating will be done. Reason: " + e.getLocalizedMessage());
+                        e.printStackTrace();
+                        System.err.println();
+                    }});
+                toRun.start();
             }
         }
     }
